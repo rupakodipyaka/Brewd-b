1c5042d1a19c6dca0a8db10da92db14ee7ce673a
Hammad Tahir <tahirh@deakin.edu.au>
2018-08-07 08:18:28 +1000

    d6

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose');
 // set up express app
 const app = express();
 
+console.log('1');
+
 // connect to mongodb
 mongoose.connect('mongodb://user:pass@host:port/dbname',function(err,db){
         if(err){
@@ -13,23 +15,32 @@ mongoose.connect('mongodb://user:pass@host:port/dbname',function(err,db){
            console.log('connected to the Test db');
        }
      });
+
+console.log('2');
 mongoose.Promise = global.Promise;
+console.log('3');
 
 //set up static files
 app.use(express.static('public'));
 
+console.log('4');
+
 // use body-parser middleware
 app.use(bodyParser.json());
 
+console.log('5');
+
 // initialize routes
 app.use('/api', require('./routes/api'));
+
+console.log('6');
 
 // error handling middleware
 app.use(function(err, req, res, next){
     console.log(err); // to see properties of message in our console
     res.status(422).send({error: err.message});
 });
-
+console.log('7');
 // listen for requests
 app.listen(process.env.port || 3000, function(){
     console.log('now listening for requests');
